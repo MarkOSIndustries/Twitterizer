f374d590b23644d7c7b36547f005756f78a815e7
Mark Green <markg@campaignmonitor.com>
2014-04-07 10:25:40 +1000

    CM-specific hack - expose OAuth token info for use in logging

--- Twitterizer2/OAuth/OAuthTokens.cs
+++ Twitterizer2/OAuth/OAuthTokens.cs
@@ -44,25 +44,25 @@ namespace Twitterizer
         /// Gets or sets the access token.
         /// </summary>
         /// <value>The access token.</value>
-        public string AccessToken { internal get; set; }
+        public string AccessToken { get; set; }
 
         /// <summary>
         /// Gets or sets the access token secret.
         /// </summary>
         /// <value>The access token secret.</value>
-        public string AccessTokenSecret { internal get; set; }
+        public string AccessTokenSecret { get; set; }
 
         /// <summary>
         /// Gets or sets the consumer key.
         /// </summary>
         /// <value>The consumer key.</value>
-        public string ConsumerKey { internal get; set; }
+        public string ConsumerKey { get; set; }
 
         /// <summary>
         /// Gets or sets the consumer secret.
         /// </summary>
         /// <value>The consumer secret.</value>
-        public string ConsumerSecret { internal get; set; }
+        public string ConsumerSecret { get; set; }
 
         /// <summary>
         /// Gets a value indicating whether this instance has consumer token values.
